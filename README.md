c647de6895e856c2b9ed2e2f067eb58a66cb0bb7
Stephen Jurgensen <sdjurgy@gmail.com>
2022-10-09 13:42:53 -0700

    Configure React app for deployment to GitHub Pages

--- README.md
+++ README.md
@@ -1,46 +1,70 @@
-# Getting Started with Create React App and Redux
+# Project: markdown viewer  
+This is a web development project that uses the front-end framework React.  
+Redux is used to manage state. It is in partial completion of freeCodeCamp's   
+front-end framework certificate program.  
+  
+## Live (gh-pages) Link:  
+  https://jurgensen1.github.io/javascript-calculator-react/
+  
+## Requirements:   
+Use any mix of HTML, JavaScript, CSS, Bootstrap, SASS, React, Redux, and jQuery  
+to complete this project.   
+  
+Fulfill the below user stories:  
+  
+* User Story #1: My calculator should contain a clickable element containing an  
+= (equal sign) with a corresponding id="equals".
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
+* User Story #2: My calculator should contain 10 clickable elements containing  
+one number each from 0-9, with the following corresponding IDs: id="zero",  
+id="one", id="two", id="three", id="four", id="five", id="six", id="seven",  
+id="eight", and id="nine".  
+  
+* User Story #3: My calculator should contain 4 clickable elements each  
+containing one of the 4 primary mathematical operators with the following  
+corresponding IDs: id="add", id="subtract", id="multiply", id="divide".  
+  
+* User Story #4: My calculator should contain a clickable element containing a  
+. (decimal point) symbol with a corresponding id="decimal".  
+  
+* User Story #5: My calculator should contain a clickable element with an  
+id="clear".
 
-## Available Scripts
+* User Story #6: My calculator should contain an element to display values with  
+a corresponding id="display".  
+  
+* User Story #7: At any time, pressing the clear button clears the input and  
+output values, and returns the calculator to its initialized state; 0 should  
+be shown in the element with the id of display.  
+  
+* User Story #8: As I input numbers, I should be able to see my input in the  
+element with the id of display.  
+  
+* User Story #9: In any order, I should be able to add, subtract, multiply and  
+divide a chain of numbers of any length, and when I hit =, the correct result  
+should be shown in the element with the id of display.  
+  
+* User Story #10: When inputting numbers, my calculator should not allow a  
+number to begin with multiple zeros.  
+  
+* User Story #11: When the decimal element is clicked, a . should append to  
+the currently displayed value; two . in one number should not be accepted.  
+  
+* User Story #12: I should be able to perform any operation (+, -, *, /) on  
+numbers containing decimal points.  
+  
+* User Story #13: If 2 or more operators are entered consecutively, the  
+operation performed should be the last operator entered (excluding the  
+negative (-) sign). For example, if 5 + * 7 = is entered, the result should  
+be 35 (i.e. 5 * 7); if 5 * - 5 = is entered, the result should be -25 (i.e.  
+5 * (-5)).  
+  
+* User Story #14: Pressing an operator immediately following = should start  
+a new calculation that operates on the result of the previous evaluation.  
+  
+* User Story #15: My calculator should have several decimal places of   
+precision when it comes to rounding (note that there is no exact standard,   
+but you should be able to handle calculations like 2 / 7 with reasonable   
+precision to at least 4 decimal places).  
 
-In the project directory, you can run:
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,230 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
+    computeString,
+    handleChange,
+    answerChange,
+    bufferClear,
+    inputBufferClear,
+    clearAll,
+    selectAnswer,
+    selectInputString,
+    selectBuffer,
+    selectInputBuffer,
+    selectEqualsBo
 } from './counterSlice';
 import styles from './Counter.module.css';
 
 export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+    const answer = useSelector(selectAnswer);
+    const inputString = useSelector(selectInputString);
+    const buffer = useSelector(selectBuffer);
+    const inputBuffer = useSelector(selectInputBuffer);
+    const equalsBo = useSelector(selectEqualsBo);
+    const dispatch = useDispatch();
 
-  const incrementValue = Number(incrementAmount) || 0;
-
-  return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <div id={styles.displayWrapper}>
+                    <div id='stringDisplay' className={styles.display}>{inputString ? inputString : "0"}</div>
+                    <div id='display' className={styles.display}>{answer}</div>
+                </div>
+                <div id={styles.buttonsWrapper}>
+                    <button
+                            className={styles.buttonWide}
+                            aria-label="clear"
+                            id="clear"
+                            onClick={(e) => {
+                                dispatch(clearAll());
+                            }}
+                        >
+                            AC
+                    </button>
+                    <button
+                        className={styles.buttonWide}
+                        aria-label="divide"
+                        id="divide"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '/'));
+                            dispatch(answerChange('/'));
+                            dispatch(bufferClear());
+                        }}
+                    >
+                        /
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="seven"
+                        id="seven"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '7'));
+                            dispatch(answerChange(buffer + '7'));
+                        }}
+                    >
+                        7
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="eight"
+                        id="eight"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '8'));
+                            dispatch(answerChange(buffer + '8'));
+                        }}
+                    >
+                        8
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="nine"
+                        id="nine"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputString + '9'));
+                            dispatch(answerChange(buffer + '9'));
+                        }}
+                    >
+                        9
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="multiply"
+                        id="multiply"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '*'));
+                            dispatch(answerChange('*'));
+                            dispatch(bufferClear());
+                        }}
+                    >
+                        *
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="four"
+                        id="four"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '4'));
+                            dispatch(answerChange(buffer + '4'));
+                        }}
+                    >
+                        4
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="five"
+                        id="five"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '5'));
+                            dispatch(answerChange(buffer + '5'));
+                        }}
+                    >
+                        5
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="six"
+                        id="six"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '6'));
+                            dispatch(answerChange(buffer + '6'));
+                        }}
+                    >
+                        6
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="subtract"
+                        id="subtract"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '-'));
+                            dispatch(answerChange('-'));
+                            dispatch(bufferClear());
+                        }}
+                    >
+                        -
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="one"
+                        id="one"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '1'));
+                            dispatch(answerChange(buffer + '1'));
+                        }}
+                    >
+                        1
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="two"
+                        id="two"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '2'));
+                            dispatch(answerChange(buffer + '2'));
+                        }}
+                    >
+                        2
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="three"
+                        id="three"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '3'));
+                            dispatch(answerChange(buffer + '3'));
+                        }}
+                    >
+                        3
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="add"
+                        id="add"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '+'));
+                            dispatch(answerChange('+'));
+                            dispatch(bufferClear());
+                        }}
+                    >
+                        +
+                    </button>
+                    <button
+                        className={styles.buttonWide}
+                        aria-label="zero"
+                        id="zero"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '0'));
+                            dispatch(answerChange(buffer + '0'));
+                        }}
+                    >
+                        0
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="decimal"
+                        id="decimal"
+                        onClick={(e) => {
+                            dispatch(handleChange(inputBuffer + '.'));
+                            dispatch(answerChange(buffer + '.'));
+                        }}
+                    >
+                        .
+                    </button>
+                    <button
+                        className={styles.button}
+                        aria-label="equals"
+                        id="equals"
+                        onClick={() => {
+                            dispatch(computeString(inputString));
+                            dispatch(handleChange(inputBuffer + "="));
+                            dispatch(inputBufferClear());
+                            dispatch(bufferClear());
+                        }}
+                    >
+                        =
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
